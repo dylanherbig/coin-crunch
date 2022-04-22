07c25f8c936f5c328748431c1c9d6b35d8695c17
dylanherbig <83849210+dylanherbig@users.noreply.github.com>
2022-04-22 15:43:41 -0400

    updated urls.py

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
+    path('configuration/', views.configuration, name='configuration'),
+    path('balances/', views.balances, name='balances'),
+    path('create/', views.createWallet, name='createUser'),
+    path('user-wallet/', views.getUserWallet, name='getUserWallet'),
+    path('create-blockchain-address/', views.createBlockchainAddress, name='createBlockchainAddress'),
     path(r'', views.default_map, name="default"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
